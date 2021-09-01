e25be8542508c35524fd4d93e6533731a8c28c56
ak-empereur-7 <41908463+ak-empereur-7@users.noreply.github.com>
2021-09-01 21:52:20 +0900

    use FC VFC

---
+++ src/Text.tsx
@@ -0,0 +1,11 @@
+import { VFC } from "react";
+
+interface Props {
+  color: string;
+  fontSize: string;
+}
+
+export const Text: VFC<Props> = (props) => {
+  const { color, fontSize } = props;
+  return <p style={{ color, fontSize }}>テキストです</p>;
+};
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,8 +1,9 @@
+import { VFC } from "react";
 import { TodoType } from "./type/todo";
 
 // Pick<TodoType, "userId", "title", "completed">で必要なプロパティを取る
 // Omitは除く
-export const Todo = (props: Omit<TodoType, "id">) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark}${title}(ユーザーID：${userId})`}</p>;
