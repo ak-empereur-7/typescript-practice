b5202b4814a3054d87e3059f1bee1e77d3369385
ak-empereur-7 <41908463+ak-empereur-7@users.noreply.github.com>
2021-09-02 06:29:28 +0900

    create User

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./type/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./type/user";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
 
+  const user: User = {
+    name: "Dan",
+    hobbies: ["英語", "映画"]
+  };
   const onClickFetchData = () => {
     axios
       .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
@@ -17,6 +23,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
